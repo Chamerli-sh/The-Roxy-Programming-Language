183bc410086753416798344bd272be17d3fb8468
Chamerli-sh <fares.setbels@gmail.com>
2022-03-13 20:37:51 +0100

    Just to be sure

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,14 @@ impl Lexer {
 fn main() {
     let file = env::args().nth(1).unwrap();
 
-    let content = fs::read_to_string(file).unwrap();
+    let not_sure_content = fs::read_to_string(file);
+
+    let content = if (not_sure_content.is_ok()) {
+        not_sure_content.unwrap()
+    } else {
+        panic!("Could not read file :)).");
+    };
+
     println!("{}", content);
 
     let lexer = Lexer::new(content);
