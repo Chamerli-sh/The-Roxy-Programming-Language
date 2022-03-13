14ff7803673fd38e8fa98d659f7fccd7e7c5dfb8
Chamerli-sh <fares.setbels@gmail.com>
2022-03-13 20:18:57 +0100

    File Reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{fs, env};
+
 fn main() {
-    println!("Hello, world!");
+    let file = env::args().nth(1).unwrap();
+
+    let contents = fs::read_to_string(file).unwrap();
+    println!("{}", contents);
 }
